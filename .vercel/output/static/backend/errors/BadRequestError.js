cca6edb73ef473584651ebabc3fb0658eccb68b5
Ilya Novik <ilya.novikk@gmail.com>
2023-06-05 17:23:56 +0200

    refactor: fix gitignore

--- .vercel/output/static/backend/errors/BadRequestError.js
+++
@@ -1,9 +0,0 @@
-class BadRequestError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'badRequest';
-    this.statusCode = 400;
-  }
-}
-
-module.exports = BadRequestError;
--- .vercel/output/static/backend/errors/ServerError.js
+++
@@ -1,9 +0,0 @@
-class ServerError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'serverError';
-    this.statusCode = 500;
-  }
-}
-
-module.exports = ServerError;
